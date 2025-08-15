e7b4abef1dcdac21c4a9e8e3ea366cdc17108196
Siddharth Darkunde <siddhartdarkunde2444@gmail.com>
2025-08-15 16:35:39 +0530

    Add files via upload

---
+++ GmblsApplication.java
@@ -0,0 +1,13 @@
+package com.sd.gmbls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmblsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmblsApplication.class, args);
+	}
+
+}
